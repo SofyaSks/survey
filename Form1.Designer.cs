8c64ee2334b9f4a703c28c2af753498802576750
sks <ssurovneva@mail.ru>
2023-04-06 22:02:03 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace survey
+{
+    partial class Survey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox_country = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_country = new System.Windows.Forms.Label();
+            this.label_surname = new System.Windows.Forms.Label();
+            this.label_phone = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label_gender = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button_show = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label_gender);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.label_phone);
+            this.groupBox1.Controls.Add(this.comboBox_country);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label_country);
+            this.groupBox1.Controls.Add(this.label_surname);
+            this.groupBox1.Location = new System.Drawing.Point(69, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(542, 300);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "survey";
+            // 
+            // comboBox_country
+            // 
+            this.comboBox_country.FormattingEnabled = true;
+            this.comboBox_country.Items.AddRange(new object[] {
+            "германия",
+            "россия",
+            "украина",
+            "болгария"});
+            this.comboBox_country.Location = new System.Drawing.Point(196, 123);
+            this.comboBox_country.Name = "comboBox_country";
+            this.comboBox_country.Size = new System.Drawing.Size(199, 28);
+            this.comboBox_country.TabIndex = 3;
+            this.comboBox_country.SelectedIndexChanged += new System.EventHandler(this.comboBox_country_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label_country
+            // 
+            this.label_country.AutoSize = true;
+            this.label_country.Location = new System.Drawing.Point(53, 123);
+            this.label_country.Name = "label_country";
+            this.label_country.Size = new System.Drawing.Size(61, 20);
+            this.label_country.TabIndex = 1;
+            this.label_country.Text = "country";
+            // 
+            // label_surname
+            // 
+            this.label_surname.AutoSize = true;
+            this.label_surname.Location = new System.Drawing.Point(53, 58);
+            this.label_surname.Name = "label_surname";
+            this.label_surname.Size = new System.Drawing.Size(71, 20);
+            this.label_surname.TabIndex = 0;
+            this.label_surname.Text = "surname";
+            // 
+            // label_phone
+            // 
+            this.label_phone.AutoSize = true;
+            this.label_phone.Location = new System.Drawing.Point(57, 193);
+            this.label_phone.Name = "label_phone";
+            this.label_phone.Size = new System.Drawing.Size(55, 20);
+            this.label_phone.TabIndex = 5;
+            this.label_phone.Text = "Phone";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(196, 193);
+            this.maskedTextBox1.Mask = "(999) 000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(199, 26);
+            this.maskedTextBox1.TabIndex = 6;
+            // 
+            // label_gender
+            // 
+            this.label_gender.AutoSize = true;
+            this.label_gender.Location = new System.Drawing.Point(53, 254);
+            this.label_gender.Name = "label_gender";
+            this.label_gender.Size = new System.Drawing.Size(63, 20);
+            this.label_gender.TabIndex = 7;
+            this.label_gender.Text = "Gender";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(196, 249);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 24);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(313, 249);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(82, 24);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button_show
+            // 
+            this.button_show.Location = new System.Drawing.Point(310, 369);
+            this.button_show.Name = "button_show";
+            this.button_show.Size = new System.Drawing.Size(107, 43);
+            this.button_show.TabIndex = 10;
+            this.button_show.Text = "Show";
+            this.button_show.UseVisualStyleBackColor = true;
+            this.button_show.Click += new System.EventHandler(this.button_show_Click);
+            // 
+            // Survey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_show);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Survey";
+            this.Text = "Survey";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox_country;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_country;
+        private System.Windows.Forms.Label label_surname;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label_gender;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label_phone;
+        private System.Windows.Forms.Button button_show;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace survey
+{
+    public partial class Survey : Form
+    {
+        public Survey()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox_country_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string strcountry = comboBox_country.SelectedItem.ToString();
+            // по ифам коллекцию городов
+            MessageBox.Show(strcountry);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender; // приводим к типу
+            if (rb.Checked)
+            {
+                MessageBox.Show("man");
+            }
+        }
+
+        private void button_show_Click(object sender, EventArgs e)
+        {
+            string str = "Анкета: \n";
+            if (textBox1.Text != string.Empty)
+            {
+                str += $"Surname: {textBox1.Text}\n";
+
+            }
+            if(comboBox_country.Text!= string.Empty)
+            {
+                str += comboBox_country.SelectedItem.ToString() + "\n";
+            }
+            if(maskedTextBox1.Text!= string.Empty)
+            {
+                str += maskedTextBox1.Text + "\n";
+            }
+            if (radioButton1.Checked == true)
+            {
+                str += $"Gender: {radioButton1.Text}\n";
+            }
+
+            MessageBox.Show(str);
+        }
+    }
+}
